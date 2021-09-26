fe9b5a03af922194adedf04ad54c0f0574a18ee2
C.Y. Tseng <diella.tseng@gmail.com>
2021-09-26 12:47:32 +0200

    init project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Connect to database
+mongoose.connect('mongodb+srv://admin:iH1Tee2EOj75f2yG@cluster0.zcrlr.mongodb.net/piiquante?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch(() => console.log('Failed to connect to MongoDB!'));
+
+// CORS (Cross-origin resource sharing) settings
+app.use(cors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+    methods: 'GET, POST, PUT, DELETE'
+}))
+
+// To parse the incoming requests with JSON payloads
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+
+
+
+module.exports = app;
