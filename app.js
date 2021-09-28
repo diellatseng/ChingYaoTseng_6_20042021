a3735918a8f5d7ac6fe49bf14d3c73e80765a0d3
C.Y. Tseng <diella.tseng@gmail.com>
2021-09-28 09:49:57 +0200

    add user ctrl and route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 //Connect to database
@@ -12,16 +14,12 @@ mongoose.connect('mongodb+srv://admin:iH1Tee2EOj75f2yG@cluster0.zcrlr.mongodb.ne
     .catch(() => console.log('Failed to connect to MongoDB!'));
 
 // CORS (Cross-origin resource shring) settings
-app.use(cors({
-    origin: '*',
-    allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
-    methods: 'GET, POST, PUT, DELETE'
-}))
+app.use(cors());
 
 // To parse the incoming requests with JSON payloads
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 
-
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,43 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.signup = (req, res) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            })
+            user.save()
+                .then(() => res.status(201).json({ message: "User created!" }))
+                .catch(error => res.status(400).json({ error }))
+        })
+        .catch(error => res.status(500).json({ error }))
+};
+
+exports.login = (req, res, next) => {
+    console.log('hello');
+    User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'User not found!' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Password incorrect!' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id},
+              'RANDOM-SECRET-TOKEN',
+              { expiresIn: '1h' }
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
