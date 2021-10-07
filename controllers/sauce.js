7bac688a9b7e7fc5f3d7ba19fb26faf38ec2fab9
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-07 11:26:04 +0200

    fix bug: update sauce with new photo, old photo was not deleted

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -14,12 +14,26 @@ exports.createSauce = (req, res) => {
 };
 
 exports.modifySauce = (req, res) => {
-  const sauceObject = req.file ? 
-  {
-    ...JSON.parse(req.body.sauce),
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  } : {...req.body};
-
+  let sauceObject;
+  if (req.file) {
+    // If new image is added, delete old file and save new file
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          console.log(`images/${filename} was deleted`);
+        })
+      })
+      .then(sauceObject = {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      })
+      .catch(error => res.status(400).json({ message: error.message }));
+  } else {
+    // If no new image is added, update only req.body
+    sauceObject = {...req.body};
+  }
+  
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
     .catch(error => res.status(400).json({ message: error.message }));
