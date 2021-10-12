3f7e8d5178f9cfc3e8d5507320d05d6faddf777a
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-12 11:25:18 +0200

    add multer fileFilter to control uploaded file types

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,33 +1,33 @@
 const multer = require('multer');
+const path = require('path');
 
 // Accepts only the following types of images
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
+const validMimeTypes= [
+  'image/jpg',
+  'image/jpeg',
+  'image/png'
+]
 
-// Rename uploaded file and save the file
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, './images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.toLowerCase().split(' ').join('_');    //make sure that any space in file name will be replaced by "_"
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+// Multer option object
+const saveImageToStorage = {
+  storage: multer.diskStorage({
+    // Set the folder to which the file has been saved
+    destination: (req, file, callback) => { callback(null, './images') },
+    // Rename file
+    filename: (req, file, callback) => {
+      const name = file.originalname.toLowerCase().split(' ').join('_');                      //make sure that any space in file name will be replaced by "_"
+      const extension = path.extname(file.originalname);
+      callback(null, name + Date.now() + extension);
+    }
+  }),
+  // Function to control which files are accepted
+  fileFilter: (req, file, callback) => {
+    if (validMimeTypes.includes(file.mimetype)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Invalid file type! Accepts only .png, .jpg and .jpeg extensions.'), false);
+    }
   }
-})
+}
 
-// const fileFilter = (req, file, callback) => {
-//   console.log(file.mimetype);
-//   if (file.mimetype == "image/png" || file.mimetype == "image/jpg") {
-//     callback(null, true);
-//   } else {
-//     callback(null, false);
-//   }
-// }
-
-module.exports = multer({
-  storage: storage
-}).single('image');
+module.exports = multer(saveImageToStorage).single('image');
